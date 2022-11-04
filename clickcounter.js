3c7e2781131a6e0ec2c5fe58567b2bb0046b3146
RonitR10 <115337649+RonitR10@users.noreply.github.com>
2022-11-04 23:43:03 +0530

    Add files via upload

---
+++ clickcounter.js
@@ -0,0 +1,12 @@
+const countBTn = document.querySelector('countBtn button');
+let countNUM = document.querySelector('.countNUm h1')
+
+countBTn.addEventListener('click', countUp);
+
+function countUp(){
+    countNum.innerHTML++;
+}
+function reset(){
+    countNum.innerHTML=0;
+}
+}
